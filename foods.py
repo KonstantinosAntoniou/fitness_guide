276a034f40af53904e4dedee0f523302de90a470
Konstantinos Antoniou <kostantinosantoniou@192.168.1.19>
2025-04-18 18:13:03 +0300

    Add load from excel functionality, change the foods, meals and app files for the database scheme

--- foods.py
+++ foods.py
@@ -1,10 +1,11 @@
-import pandas as pd
-import os
-from openpyxl import load_workbook
-from openpyxl.styles import PatternFill
+from sqlalchemy.orm import Session
+from db import SessionLocal
+from models import Food as FoodModel
 
 class Food:
-    def __init__(self, name=None, label=None, measurement=None, calories = None, protein=None, carbs=None, fat_saturated=None, fat_regular=None, sodium=None):
+    def __init__(self, name: str, label: str, measurement: str,
+                 calories: float, protein: float, carbs: float,
+                 fat_saturated: float, fat_regular: float, sodium: float):
         self.name = name
         self.label = label
         self.measurement = measurement
@@ -14,48 +15,30 @@ class Food:
         self.fat_saturated = fat_saturated
         self.fat_regular = fat_regular
         self.sodium = sodium
+        self.db: Session = SessionLocal()
 
-        # The Excel file where food data is stored
-        self.excel_file = 'foods_log.xlsx'
-        
-        # Ensure the file exists with the proper columns
-        if not os.path.exists(self.excel_file):
-            self.create_excel_file()
+    def log_food(self) -> str:
+        exists = self.db.query(FoodModel).filter(
+            FoodModel.name.ilike(self.name),
+            FoodModel.label.ilike(self.label)
+        ).first()
+        if exists:
+            return f"{self.name} ({self.label}) already exists in database."
 
-    def create_excel_file(self):
-        columns = ['Name', 'Label', 'Measurement', 'Calories', 'Protein', 'Carbs', 'Fat_Saturated', 'Fat_Regular', 'Sodium']
-        df = pd.DataFrame(columns=columns)
-        df.to_excel(self.excel_file, index=False)
+        new_food = FoodModel(
+            name=self.name,
+            label=self.label,
+            measurement=self.measurement,
+            calories=self.calories,
+            protein=self.protein,
+            carbs=self.carbs,
+            fat_saturated=self.fat_saturated,
+            fat_regular=self.fat_regular,
+            sodium=self.sodium
+        )
+        self.db.add(new_food)
+        self.db.commit()
+        return f"{self.name} ({self.label}) logged successfully to database!"
 
-    def log_food(self):
-        df = pd.read_excel(self.excel_file)
-        if not ((df['Name'].str.lower() == self.name.lower()) & (df['Label'].str.lower() == self.label.lower())).any():
-            new_food = {
-                'Name': self.name,
-                'Label': self.label,
-                'Measurement': self.measurement,
-                'Calories' : self.calories,
-                'Protein': self.protein,
-                'Carbs': self.carbs,
-                'Fat_Saturated': self.fat_saturated,
-                'Fat_Regular': self.fat_regular,
-                'Sodium': self.sodium
-            }
-            df = df._append(new_food, ignore_index=True)
-            df.to_excel(self.excel_file, index=False)
-            return f"{self.name} ({self.label}) logged successfully!"
-        else:
-            return f"{self.name} ({self.label}) is already logged."
-
-    def print_all_foods(self):
-        df = pd.read_excel(self.excel_file)
-        print(df)
-
-    def get_length(self):
-        df = pd.read_excel(self.excel_file)
-        return len(df)
-
-    def print_names_labels(self):
-        df = pd.read_excel(self.excel_file)
-        for index, row in df.iterrows():
-            print(f"Name: {row['Name']}, Label: {row['Label']}")
+    def get_all_foods(self):
+        return self.db.query(FoodModel).all()
---
+++ load_from_excel.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from db import SessionLocal, init_db
+
+from models import Food as FoodModel, Meal as MealModel, MealFood as MealFoodMeal
+
+def load_foods(excel_path: str = 'food_log.xlsx'):
+
+    session = SessionLocal()
+    df = pd.read_excel(excel_path)
+
+    for index, row  in df.iterrows():
+        existing = session.query(FoodModel).filter(
+            FoodModel.name.ilike(row['Name']),
+            FoodModel.label.ilike(row['Label'])
+        ).first()
+        if not existing:
+            new_food = FoodModel(
+                name=row['Name'],
+                label=row['Label'],
+                measurement=row['Measurement'],
+                calories=row['Calories'],
+                protein=row['Protein'],
+                carbs=row['Carbs'],
+                fat_saturated=row['Fat_Saturated'],
+                fat_regular=row['Fat_Regular'],
+                sodium=row['Sodium']
+            )
+            session.add(new_food)
+    
+    session.commit()
+    session.close()
+    print("Foods loaded from Excel file.")
+
+def load_meals(excel_path: str = 'meals_log.xlsx'):
+    session = SessionLocal()
+    df = pd.read_excel(excel_path)
+    grouped = df.groupby('Meal_Name')
+    for meal_name, group in grouped:
+        meal = MealModel(name=meal_name)
+        session.add(meal)
+        session.flush()
+
+        foods = group[~group['Food_Name'].str.contains('Total')]
+
+        for index, row in foods.iterrows():
+            mult, food_name = row['Food_Name'].split('x', 1)
+            multiplier = float(mult)
+            label = row['Label']
+            food = session.query(FoodModel).filter(
+                FoodModel.name.ilike(food_name),
+                FoodModel.label.ilike(label)
+            ).first()
+            if food:
+                meal_food = MealFoodMeal(
+                    meal_id=meal.id,
+                    food_id=food.id,
+                    multiplier=multiplier
+                )
+                session.add(meal_food)
+    session.commit()
+    session.close()
+    print("Meals loaded from Excel file.")
+
+def main():
+    init_db()
+    load_foods()
+    load_meals()
+    print("Data migraded from Excel to database.")
+
+if __name__ == "__main__":
+    main()
